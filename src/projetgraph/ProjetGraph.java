d78012fe7f88733fbef853c82cba6f5e4b7738b2
Cebrail Yalcin <Cebrail Yalcin@DESKTOP-19OSDRT>
2018-05-16 15:47:17 +0200

    Initialisation

---
+++ src/projetgraph/ProjetGraph.java
@@ -0,0 +1,17 @@
+
+package projetgraph;
+
+/**
+ *
+ * @author Cebrail Yalcin
+ */
+public class ProjetGraph {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
